745bd774484b4c9c37ff9876fbb599f7c29284b2
Jeremy 'LeJyBy' Barbay <jeremy@barbay.cl>
2016-03-26 21:45:24 -0300

    Created first test.

---
+++ groupDockMerge.py
@@ -0,0 +1,37 @@
+import unittest, doctest, copy
+
+
+def gdm(frequencies):
+    """Implementation in Python of the "Group Dock and Merge" algorithm.
+
+       This algorithm is supposed to compute optimal prefix free codes in time o(n lg n) for various classes of instances.  
+
+       It receives as input an array of integer frequencies of symbols.
+       It returns
+          - an array partially sorting those frequencies and
+          - an array of the same size containing the associated code lengths forming an optimal prefix free code for those frequencies.
+
+    """
+
+
+
+class GDMTest(unittest.TestCase):
+    """Basic tests for the GDM algorithm computing optimal prefix free codes.
+
+    """
+        
+    def testTrivialInstance(self):
+        """Test what happens on an empty Instance.
+        """
+        self.assertEqual(gdm([]),([],[]))
+
+    
+    
+def main():
+    unittest.main()
+if __name__ == '__main__':
+    doctest.testmod()
+    main()
+            
+        
+
