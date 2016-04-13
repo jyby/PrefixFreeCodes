8a734cc9151891d76dbfc0100b907402e6d2296d
Jeremy 'LeJyBy' Barbay <jeremy@barbay.cl>
2016-04-13 15:26:49 -0300

    Refactorized gdmCodeTree into subfunctions for better readability
    and testability.

--- gdm.py
+++ gdm.py
@@ -98,58 +98,47 @@ class InternalNode:
     def __str__(self):
         string = "("+str(self.CachedValueOfWeight)+","+str(self.left)+","+str(self.right)+")"
         return string
-    
-def gdmCodeTree(frequencies):
-    """Given a partially sorted list of weights, return a code tree of minimal
-    redundancy according to the GDM algorithm.
+
+def DOCK(frequencies,nodes,nbFrequenciesProcessed):
+    """Given a set of internal nodes, group them two by two until at least one internal node has weight larger than the weight of the next External node (but smaller than twice this weight)
+
     """
-    if len(frequencies) == 0 :
-        return None
-    elif len(frequencies)==1:
-        return ExternalNode(frequencies,0)
-    # INITIALIZATION
-    nodes = [InternalNode(frequencies,ExternalNode(frequencies,0),ExternalNode(frequencies,1))] 
-    nbFrequenciesProcessed = 2
-    while nbFrequenciesProcessed < len(frequencies):
-        # GROUP weights of similar weights: 
-        r = frequencies.rankRight(nodes[0].weight())        
-        if r == nbFrequenciesProcessed:
-            nodes[0].weight()
-            nodes = nodes[1:]+[InternalNode(frequencies,nodes[0],ExternalNode(frequencies,r))]
-            nbFrequenciesProcessed += 1
-        else:
-            if (r-nbFrequenciesProcessed) % 2 == 1:
-                nodes = [ExternalNode(frequencies,r-1)]+nodes
-            for i in range((r-nbFrequenciesProcessed)//2):
-                left = ExternalNode(frequencies,nbFrequenciesProcessed+2*i)
-                right = ExternalNode(frequencies,nbFrequenciesProcessed+2*i+1)        
-                nodes.append(InternalNode(frequencies,left,right))
-            nbFrequenciesProcessed = r
-        if nbFrequenciesProcessed == len(frequencies):
-            break
-        # print(str(nbFrequenciesProcessed)+" frequencies processed out of "+str(len(frequencies)))
-        # DOCK current nodes to the level of the next External node
-        # print("First available external has weight "+str(frequencies.select(nbFrequenciesProcessed))+", while the largest internal node has weight "+str(nodes[-1].weight()))
-        while len(nodes)>1 and nodes[-1].weight() <= frequencies.select(nbFrequenciesProcessed):
-            # print(str(len(nodes))+" nodes left, of maximal weight "+str(nodes[-1].weight(),))
-            nbPairsToForm = len(nodes)//2
-            for i in range(nbPairsToForm):
-                nodes.append(InternalNode(frequencies,nodes[0],nodes[1]))
-                nodes = nodes[2:]
-        # MERGE the internal nodes with the external nodes of similar weights
-        nbInternals = len(nodes)
-        for i in range(nbInternals):
-            r = frequencies.rankRight(nodes[0].weight())
-            nbPairsToForm = (nbFrequenciesProcessed-r) // 2
-            for j in range(nbPairsToForm):
-                nodes.append(InternalNode(frequencies,ExternalNode(frequencies,nbFrequenciesProcessed),ExternalNode(frequencies,nbFrequenciesProcessed+1)))
-                nbFrequenciesProcessed += 2
-            # !!!! Need some code here to combine any quantity of internal nodes if relevant !!!!!!!     
-            nodes.append(InternalNode(frequencies,nodes[0],ExternalNode(frequencies,nbFrequenciesProcessed)))
-            nodes = nodes[1:]
-            nbFrequenciesProcessed += 1
-            
-    # WRAP-UP when there are only internal nodes left.
+    # print(str(nbFrequenciesProcessed)+" frequencies processed out of "+str(len(frequencies)))
+    # print("First available external has weight "+str(frequencies.select(nbFrequenciesProcessed))+", while the largest internal node has weight "+str(nodes[-1].weight()))
+    while len(nodes)>1 and nodes[-1].weight() <= frequencies.select(nbFrequenciesProcessed):
+        # print(str(len(nodes))+" nodes left, of maximal weight "+str(nodes[-1].weight(),))
+        nbPairsToForm = len(nodes) // 2
+        for i in range(nbPairsToForm):
+            nodes.append(InternalNode(frequencies,nodes[0],nodes[1]))
+            nodes = nodes[2:]
+    return frequencies,nodes,nbFrequenciesProcessed
+
+def MERGE(frequencies,nodes,nbFrequenciesProcessed):
+    """Merge the list of Internal nodes with the external nodes of similar weight.
+
+"""
+    internalNodesToMerge = nodes
+    externalNodesToMerge = []
+    for p in range(nbFrequenciesProcessed,frequencies.rankRight(nodes[-1].weight())):
+        externalNodesToMerge.append(ExternalNode(frequencies,p))
+        nbFrequenciesProcessed += 1
+    nodes = []
+    while(len(internalNodesToMerge)>0 and len(externalNodesToMerge)>0):
+        children = []
+        for i in range(2):
+            if len(externalNodesToMerge)==0 or internalNodesToMerge[0].weight() < externalNodesToMerge[0].weight() :
+                children.append(internalNodesToMerge[0])
+                internalNodesToMerge = internalNodesToMerge[1:]
+            else:
+                children.append(externalNodesToMerge[0])
+                externalNodesToMerge = internalNodesToMerge[1:]
+        nodes.append(InternalNode(frequencies,children[0],children[1]))
+    nodes = internalNodesToMerge + externalNodesToMerge + nodes
+    return frequencies,nodes,nbFrequenciesProcessed
+
+def WRAPUP(frequencies,nodes):
+    """Combine the internal nodes of a list until only one is left.
+"""
     while len(nodes) > 1:
         if len(nodes) % 2 == 1:
             nodes[-1].weight()
@@ -157,13 +146,56 @@ def gdmCodeTree(frequencies):
             nodes.append(InternalNode(frequencies,nodes[0],nodes[1]))
             nodes = nodes[2:]
     nodes[0].weight()
+    return frequencies,nodes
+
+def gdmCodeTree(frequencies):
+    """Given a partially sorted list of weights, return a code tree of minimal
+    redundancy according to the GDM algorithm.
+
+    >>> print(gdmCodeTree(PartiallySortedArray([1,1,1,1])))
+    (4,(2,[None],[None]),(None,[None],[None]))
+    >>> print(gdmCodeTree(PartiallySortedArray([1,2,4,8,16,32,64,128,256])))
+    (511,(255,(127,(63,(31,(15,(7,(3,[None],[None]),[None]),[None]),[None]),[None]),[None]),[None]),[None])
+    """
+    if len(frequencies) == 0 :
+        return None
+    elif len(frequencies)==1:
+        return ExternalNode(frequencies,0)
+    ### INITIALIZATION
+    nodes = [InternalNode(frequencies,ExternalNode(frequencies,0),ExternalNode(frequencies,1))] 
+    nbFrequenciesProcessed = 2
+    while nbFrequenciesProcessed < len(frequencies):
+        ### GROUP weights of similar weights: 
+        r = frequencies.rankRight(nodes[0].weight())        
+        if len(nodes)==1 and r == nbFrequenciesProcessed: # if there is only one internal node and it is smaller than any external node
+            nodes[0].weight()
+            nodes = nodes[1:]+[InternalNode(frequencies,nodes[0],ExternalNode(frequencies,r))]
+            nbFrequenciesProcessed += 1
+        else:
+            if (r-nbFrequenciesProcessed) % 2 == 1: # if there is an odd number of external nodes smaller than the smallest internal node,
+                nodes = [ExternalNode(frequencies,r-1)]+nodes # promote the last external node by adding it directly to the list of nodes.
+            for i in range((r-nbFrequenciesProcessed)//2): # pair the even number of nodes preceding it, andd add them to the list of nodes.
+                left = ExternalNode(frequencies,nbFrequenciesProcessed+2*i)
+                right = ExternalNode(frequencies,nbFrequenciesProcessed+2*i+1)        
+                nodes.append(InternalNode(frequencies,left,right))
+            nbFrequenciesProcessed = r
+        if nbFrequenciesProcessed == len(frequencies):
+            break
+        frequencies,nodes,nbFrequenciesProcessed = DOCK(frequencies,nodes,nbFrequenciesProcessed)
+        frequencies,nodes,nbFrequenciesProcessed = MERGE(frequencies,nodes,nbFrequenciesProcessed)
+    frequencies,nodes = WRAPUP(frequencies,nodes)
     return nodes[0]
 
-    
-    
+
+
+
 def gdm(frequencies):
     """Given a sorted list of weights, return an array with the code lengths of an optimal prefix free code according to the GDM algorithm.
 
+    >>> print(gdm([1,1,1,1]))
+    [2, 2, 2, 2]
+    >>> print(gdm([1,2,4,8,16,32,64,128,256]))
+    [8, 8, 7, 6, 5, 4, 3, 2, 1]
     """
     # Degenerated cases
     if len(frequencies) == 0 :
