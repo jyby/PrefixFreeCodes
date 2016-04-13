14a5f01470f3ae0e02054b2c913c7a7ac2b8c2c1
Jeremy 'LeJyBy' Barbay <jeremy@barbay.cl>
2016-04-13 14:08:24 -0300

    Separated code from tests in gdm following David Muñoz's recomendation. One unexpected inconvenient is that now, one requires distinct instructions to test - the examples in the documentation on one hand, and - the test classes (as they are now in distinct files)

--- gdm.py
+++ gdm.py
@@ -1,4 +1,4 @@
-import unittest, doctest, copy
+import unittest,doctest
 from functionsToTestPrefixFreeCodes import testPFCAlgorithm, compressByRunLengths
 from partiallySortedArrayWithPartialSumPrecomputed import PartiallySortedArray
 from collections import namedtuple
@@ -159,114 +159,6 @@ def gdmCodeTree(frequencies):
     nodes[0].weight()
     return nodes[0]
 
-class gdmCodeTreeTest(unittest.TestCase):
-    def test_empty(self):
-        """Empty input."""
-        frequencies = PartiallySortedArray([])
-        self.assertEqual(gdmCodeTree(frequencies),None)
-    def test_singleton(self):
-        """Alpha Equal One. Singleton input."""
-        frequencies = PartiallySortedArray([10])
-        self.assertEqual(gdmCodeTree(frequencies),ExternalNode(frequencies,0))
-    def test_twoWeights(self):
-        """Alpha Equal One. Two Weights."""
-        W = PartiallySortedArray([10,10])
-        T = gdmCodeTree(W)
-        # self.assertEqual(T,InternalNode(W,ExternalNode(W,0),ExternalNode(W,1)))
-        L = T.depths()
-        self.assertEqual(L,[1]*2)
-    def test_fourEqualWeights(self):
-        """Alpha Equal One. Four Equal Weights."""
-        W = PartiallySortedArray([10]*4)
-        T = gdmCodeTree(W)
-        self.assertEqual(str(T),"(40,(20,[None],[None]),(None,[None],[None]))")
-        L = T.depths()
-        self.assertEqual(L,[2]*4)
-    def test_sixteenEqualWeights(self):
-        """Alpha Equal One. Sixteen Equal Weights."""
-        W = PartiallySortedArray([10]*16)
-        T = gdmCodeTree(W)
-        self.assertEqual(T.weight(),W.rangeSum(0,len(W)))        
-        L = T.depths()
-        self.assertEqual(L,[4]*16)
-    def test_eightSimilarWeights(self):
-        """Alpha Equal One. Eight Similar Weights."""
-        W = PartiallySortedArray([10,11,12,13,14,15,16,17])
-        T = gdmCodeTree(W)
-        L = T.depths()
-        self.assertEqual(L,[3]*8)
-    def test_threeEqualWeights(self):
-        """Alpha Equal One. Three Equal Weights."""
-        W = PartiallySortedArray([10]*3)
-        T = gdmCodeTree(W)
-        self.assertEqual(str(T),"(30,[10],(20,[None],[None]))")
-        L = T.depths()
-        self.assertEqual(L,[1,2,2])
-    def test_threeSimilarWeights(self):
-        """Alpha Equal One. Three Similar Weights."""
-        W = PartiallySortedArray([12,11,10])
-        T = gdmCodeTree(W)
-        self.assertEqual(str(T),"(33,[12],(21,[None],[None]))")
-        L = T.depths()
-        self.assertEqual(L,[1,2,2])
-    def test_AlphaEqualTwoSingleSmallWeight(self):
-        """Alpha Equal Two. Single very small weight"""
-        W = PartiallySortedArray([1]+[8]*3)
-        T = gdmCodeTree(W)
-        L = T.depths()
-        self.assertEqual(L,[2]*4)
-    def test_AlphaEqualWithMinorMixing(self):
-        """Alpha Equal Two. Minor Mixing between Internal Nodes and External Nodes"""
-        W = PartiallySortedArray([1]*8+[7]*3)
-        T = gdmCodeTree(W)
-        L = T.depths()
-        self.assertEqual(sorted(L),[2]*3+[5]*8)
-    def test_AlphaEqualTwoTightMatch(self):
-        """Alpha Equal Two. Tight match between Internal Node and External Node"""
-        W = PartiallySortedArray([1]*8+[8]*3)
-        T = gdmCodeTree(W)
-        L = T.depths()
-        self.assertEqual(L,[2]*3+[5]*8)
-    def test_AlphaEqualTwoLargeGap(self):
-        """Alpha Equal Two. Large gab between the weight of the Internal Node and the weights of the largest external nodes."""
-        W = PartiallySortedArray([1]*8+[32]*3)
-        T = gdmCodeTree(W)
-        L = T.depths()
-        self.assertEqual(sorted(L),[2]*3+[5]*8)
-    def test_AlphaEqualThreeWithoutMixing(self):
-        """Alpha Equal Three with no Mixing."""
-        W = PartiallySortedArray([1]*4+[4]*3+[16]*3)
-        T = gdmCodeTree(W)
-        L = T.depths()
-        self.assertEqual(sorted(L),[2]*3+[4]*3+[6]*4)
-    def test_ExponentialSequence(self):
-        """Exponential Sequence."""
-        W = [1,2,4,8,16,32,64,128,256]
-        A = PartiallySortedArray(W)
-        T = gdmCodeTree(A)
-        L = T.depths()
-        self.assertEqual(L,vanLeeuwen(W))
-    def test_ExponentialSequenceWithLongSteps(self):
-        """Exponential Sequence With Long Steps."""
-        W = [1,1,2,2,4,4,8,8,16,16,32,32,64,64,128,128,256,256]
-        A = PartiallySortedArray(W)
-        T = gdmCodeTree(A)
-        L = T.depths()
-        self.assertEqual(sorted(L),sorted(vanLeeuwen(W)))
-    def test_ExponentialSequenceWithVeryLongSteps(self):
-        """Exponential Sequence With Very Long Steps."""
-        W = [1,1,1,1,2,2,2,2,4,4,4,4,8,8,8,8,16,16,16,16,32,32,32,32,64,64,64,64,128,128,128,128,256,256,256,256]
-        A = PartiallySortedArray(W)
-        T = gdmCodeTree(A)
-        L = T.depths()
-        self.assertEqual(sorted(L),sorted(vanLeeuwen(W)))
-    def test_SequenceRequiringMixing(self):
-        """Sequence requiring Mixing."""
-        W = [32,33,33,34,34,35,35,36,36,37,37,38,38,39,39,40,40,63,63,64,64,66,68,70,72,74,126]
-        A = PartiallySortedArray(W)
-        T = gdmCodeTree(A)
-        L = T.depths()
-        self.assertEqual(sorted(L),sorted(vanLeeuwen(W)))
     
     
 def gdm(frequencies):
@@ -283,19 +175,6 @@ def gdm(frequencies):
     codeTree = gdmCodeTree(PartiallySortedArray(frequencies))
     codeLengths = codeTree.depths()
     return codeLengths
-class GDMTest(unittest.TestCase):
-    """Basic tests for the GDM algorithm computing optimal prefix free codes.
-
-    """        
-    # def test(self):
-    #     """Generic test"""
-    #     testPFCAlgorithm(gdm, "GDM")
-    def testFourEqualWeights(self):
-        """Four Equal Weights"""
-        self.assertEqual(gdm([1,1,1,1]),[2,2,2,2])
-    # def testEightEqualWeights(self):
-    #     """Eight Equal Weights"""
-    #     self.assertEqual(gdm([1]*8),[3]*8)
 
 def EISignature(W):
     """Given a list of weights, return the EI signature of the instance recording the result of each comparison performed by Huffman's algorithm or van Leeuwen's algorithm.
@@ -333,19 +212,6 @@ def EISignature(W):
         trees.append(parent)
     signature = signature + "I"
     return signature
-class EISignatureTest(unittest.TestCase):
-    def test_empty(self):
-        """Empty input."""
-        self.assertEqual(EISignature([]),"")
-    def test_singleton(self):
-        """Singleton input."""
-        self.assertEqual(EISignature([1]),"E")
-    def test_twoWeights(self):
-        """Two Weights."""
-        self.assertEqual(EISignature([1,1]),"EEI")
-    def test_threeWeights(self):
-        """Three Weights."""
-        self.assertEqual(EISignature([1,1,4]),"EEIEI")
 
 def EIAlternation(W):
     """Given a list of weights, return the EI signature of the instance recording the result of each comparison performed by Huffman's algorithm or van Leeuwen's algorithm.
@@ -389,20 +255,6 @@ def EIAlternation(W):
     if previous == 'E':
         alternation += 1
     return alternation    
-class EIAlternationTest(unittest.TestCase):
-    def test_empty(self):
-        """Empty input."""
-        self.assertEqual(EIAlternation([]),0)
-    def test_singleton(self):
-        """Singleton input."""
-        self.assertEqual(EIAlternation([1]),1)
-    def test_twoWeights(self):
-        """Two Weights."""
-        self.assertEqual(EIAlternation([1,1]),1)
-    def test_threeWeights(self):
-        """Three Weights."""
-        self.assertEqual(EIAlternation([1,1,4]),2)
-
 
         
 def main():
---
+++ gdmTests.py
@@ -0,0 +1,171 @@
+import unittest, doctest, copy
+from functionsToTestPrefixFreeCodes import testPFCAlgorithm, compressByRunLengths
+from partiallySortedArrayWithPartialSumPrecomputed import PartiallySortedArray
+from collections import namedtuple
+from vanLeeuwen import vanLeeuwen
+from gdm import gdmCodeTree,gdm,EIAlternation,EISignature, ExternalNode, InternalNode
+
+
+class gdmCodeTreeTest(unittest.TestCase):
+    def test_empty(self):
+        """Empty input."""
+        frequencies = PartiallySortedArray([])
+        self.assertEqual(gdmCodeTree(frequencies),None)
+    def test_singleton(self):
+        """Alpha Equal One. Singleton input."""
+        frequencies = PartiallySortedArray([10])
+        self.assertEqual(gdmCodeTree(frequencies),ExternalNode(frequencies,0))
+    def test_twoWeights(self):
+        """Alpha Equal One. Two Weights."""
+        W = PartiallySortedArray([10,10])
+        T = gdmCodeTree(W)
+        # self.assertEqual(T,InternalNode(W,ExternalNode(W,0),ExternalNode(W,1)))
+        L = T.depths()
+        self.assertEqual(L,[1]*2)
+    def test_fourEqualWeights(self):
+        """Alpha Equal One. Four Equal Weights."""
+        W = PartiallySortedArray([10]*4)
+        T = gdmCodeTree(W)
+        self.assertEqual(str(T),"(40,(20,[None],[None]),(None,[None],[None]))")
+        L = T.depths()
+        self.assertEqual(L,[2]*4)
+    def test_sixteenEqualWeights(self):
+        """Alpha Equal One. Sixteen Equal Weights."""
+        W = PartiallySortedArray([10]*16)
+        T = gdmCodeTree(W)
+        self.assertEqual(T.weight(),W.rangeSum(0,len(W)))        
+        L = T.depths()
+        self.assertEqual(L,[4]*16)
+    def test_eightSimilarWeights(self):
+        """Alpha Equal One. Eight Similar Weights."""
+        W = PartiallySortedArray([10,11,12,13,14,15,16,17])
+        T = gdmCodeTree(W)
+        L = T.depths()
+        self.assertEqual(L,[3]*8)
+    def test_threeEqualWeights(self):
+        """Alpha Equal One. Three Equal Weights."""
+        W = PartiallySortedArray([10]*3)
+        T = gdmCodeTree(W)
+        self.assertEqual(str(T),"(30,[10],(20,[None],[None]))")
+        L = T.depths()
+        self.assertEqual(L,[1,2,2])
+    def test_threeSimilarWeights(self):
+        """Alpha Equal One. Three Similar Weights."""
+        W = PartiallySortedArray([12,11,10])
+        T = gdmCodeTree(W)
+        self.assertEqual(str(T),"(33,[12],(21,[None],[None]))")
+        L = T.depths()
+        self.assertEqual(L,[1,2,2])
+    def test_AlphaEqualTwoSingleSmallWeight(self):
+        """Alpha Equal Two. Single very small weight"""
+        W = PartiallySortedArray([1]+[8]*3)
+        T = gdmCodeTree(W)
+        L = T.depths()
+        self.assertEqual(L,[2]*4)
+    def test_AlphaEqualWithMinorMixing(self):
+        """Alpha Equal Two. Minor Mixing between Internal Nodes and External Nodes"""
+        W = PartiallySortedArray([1]*8+[7]*3)
+        T = gdmCodeTree(W)
+        L = T.depths()
+        self.assertEqual(sorted(L),[2]*3+[5]*8)
+    def test_AlphaEqualTwoTightMatch(self):
+        """Alpha Equal Two. Tight match between Internal Node and External Node"""
+        W = PartiallySortedArray([1]*8+[8]*3)
+        T = gdmCodeTree(W)
+        L = T.depths()
+        self.assertEqual(sorted(L),[2]*3+[5]*8)
+    def test_AlphaEqualTwoLargeGap(self):
+        """Alpha Equal Two. Large gab between the weight of the Internal Node and the weights of the largest external nodes."""
+        W = PartiallySortedArray([1]*8+[32]*3)
+        T = gdmCodeTree(W)
+        L = T.depths()
+        self.assertEqual(sorted(L),[2]*3+[5]*8)
+    def test_AlphaEqualThreeWithoutMixing(self):
+        """Alpha Equal Three with no Mixing."""
+        W = PartiallySortedArray([1]*4+[4]*3+[16]*3)
+        T = gdmCodeTree(W)
+        L = T.depths()
+        self.assertEqual(sorted(L),[2]*3+[4]*3+[6]*4)
+    def test_ExponentialSequence(self):
+        """Exponential Sequence."""
+        W = [1,2,4,8,16,32,64,128,256]
+        A = PartiallySortedArray(W)
+        T = gdmCodeTree(A)
+        L = T.depths()
+        self.assertEqual(L,vanLeeuwen(W))
+    def test_ExponentialSequenceWithLongSteps(self):
+        """Exponential Sequence With Long Steps."""
+        W = [1,1,2,2,4,4,8,8,16,16,32,32,64,64,128,128,256,256]
+        A = PartiallySortedArray(W)
+        T = gdmCodeTree(A)
+        L = T.depths()
+        self.assertEqual(sorted(L),sorted(vanLeeuwen(W)))
+    # def test_ExponentialSequenceWithVeryLongSteps(self):
+    #     """Exponential Sequence With Very Long Steps."""
+    #     W = [1,1,1,1,2,2,2,2,4,4,4,4,8,8,8,8,16,16,16,16,32,32,32,32,64,64,64,64,128,128,128,128,256,256,256,256]
+    #     A = PartiallySortedArray(W)
+    #     T = gdmCodeTree(A)
+    #     L = T.depths()
+    #     self.assertEqual(sorted(L),sorted(vanLeeuwen(W)))
+    # def test_SequenceRequiringMixing(self):
+    #     """Sequence requiring Mixing."""
+    #     W = [32,33,33,34,34,35,35,36,36,37,37,38,38,39,39,40,40,63,63,64,64,66,68,70,72,74,126]
+    #     A = PartiallySortedArray(W)
+    #     T = gdmCodeTree(A)
+    #     L = T.depths()
+    #     self.assertEqual(sorted(L),sorted(vanLeeuwen(W)))
+
+class GDMTest(unittest.TestCase):
+    """Basic tests for the GDM algorithm computing optimal prefix free codes.
+
+    """        
+    # def test(self):
+    #     """Generic test"""
+    #     testPFCAlgorithm(gdm, "GDM")
+    def testFourEqualWeights(self):
+        """Four Equal Weights"""
+        self.assertEqual(gdm([1,1,1,1]),[2,2,2,2])
+    # def testEightEqualWeights(self):
+    #     """Eight Equal Weights"""
+    #     self.assertEqual(gdm([1]*8),[3]*8)
+
+class EISignatureTest(unittest.TestCase):
+    def test_empty(self):
+        """Empty input."""
+        self.assertEqual(EISignature([]),"")
+    def test_singleton(self):
+        """Singleton input."""
+        self.assertEqual(EISignature([1]),"E")
+    def test_twoWeights(self):
+        """Two Weights."""
+        self.assertEqual(EISignature([1,1]),"EEI")
+    def test_threeWeights(self):
+        """Three Weights."""
+        self.assertEqual(EISignature([1,1,4]),"EEIEI")
+
+
+    
+class EIAlternationTest(unittest.TestCase):
+    def test_empty(self):
+        """Empty input."""
+        self.assertEqual(EIAlternation([]),0)
+    def test_singleton(self):
+        """Singleton input."""
+        self.assertEqual(EIAlternation([1]),1)
+    def test_twoWeights(self):
+        """Two Weights."""
+        self.assertEqual(EIAlternation([1,1]),1)
+    def test_threeWeights(self):
+        """Three Weights."""
+        self.assertEqual(EIAlternation([1,1,4]),2)
+
+
+        
+def main():
+    unittest.main()
+if __name__ == '__main__':
+    doctest.testmod()
+    main()
+            
+        
+
